f904a961f68e3dc203a684af0a2810e5af6c7962
5war00p <maddisaiswaroop@gmail.com>
2023-06-17 12:17:16 +0530

    sidebar css

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,30 +1,34 @@
 import {
-  CalendarIcon,
-  ChartPieIcon,
-  UsersIcon,
-  Squares2X2Icon,
-  ChatBubbleLeftEllipsisIcon,
-} from "@heroicons/react/24/outline";
+  Category,
+  TaskSquare,
+  Message,
+  Profile2User,
+  Setting2,
+  AddSquare,
+  Colorfilter,
+  LampOn,
+} from "iconsax-react";
 import type { FC } from "react";
+import { ChevronDoubleLeft } from "./icons/ChevronDoubleLeft";
 
 const navigation = [
-  { name: "Home", href: "#", icon: Squares2X2Icon, count: "5", current: false },
+  { name: "Home", href: "#", icon: Category, count: "5", current: false },
   {
     name: "Messages",
     href: "#",
-    icon: ChatBubbleLeftEllipsisIcon,
+    icon: Message,
     count: "12",
     current: false,
   },
   {
     name: "Tasks",
     href: "#",
-    icon: CalendarIcon,
+    icon: TaskSquare,
     count: "20+",
     current: false,
   },
-  { name: "Members", href: "#", icon: UsersIcon, current: false },
-  { name: "Settings", href: "#", icon: ChartPieIcon, current: false },
+  { name: "Members", href: "#", icon: Profile2User, current: false },
+  { name: "Settings", href: "#", icon: Setting2, current: false },
 ];
 const teams = [
   {
@@ -63,17 +67,16 @@ function classNames(...classes: string[]) {
 
 const Sidebar: FC = () => {
   return (
-    <div className="w-[250.5px] h-screen flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
-      <div className="flex h-16 shrink-0 items-center">
-        <span className="flex gap-[9px]">
-          <img className="h-8 w-auto" src="/projectM.svg" alt="Your Company" />
-          <p className="m-auto text-xl font-semibold leading-6">Project M.</p>
-        </span>
+    <div className="w-[250.5px] h-screen flex grow flex-col gap-y-5 overflow-hidden border-r bg-white border-[#DBDBDB]">
+      <div className="flex py-8 shrink-0 items-center border-b border-[#DBDBDB] px-6">
+        <Colorfilter color="#5030E5" variant="Bulk" size={"24px"} />
+        <p className="ml-2 text-xl font-semibold leading-6">Project M.</p>
+        <ChevronDoubleLeft className="m-auto ml-[46px] h-5 w-5 text-[#787486]" />
       </div>
-      <nav className="flex flex-1 flex-col">
+      <nav className="flex flex-1 flex-col px-6">
         <ul role="list" className="flex flex-1 flex-col gap-y-7">
-          <li>
-            <ul role="list" className="-mx-2 space-y-1">
+          <li className="pb-5 border-b border-[#DBDBDB]">
+            <ul role="list" className="-mx-2 space-y-2">
               {navigation.map((item) => (
                 <li key={item.name}>
                   <a
@@ -82,7 +85,7 @@ const Sidebar: FC = () => {
                       item.current
                         ? "bg-[#5030E5] bg-opacity-[0.08] text-gray-900"
                         : "text-[#787486] hover:text-gray-900 hover:bg-[#5030E5] hover:bg-opacity-[0.08]",
-                      "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                      "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-medium"
                     )}
                   >
                     <item.icon
@@ -101,10 +104,11 @@ const Sidebar: FC = () => {
             </ul>
           </li>
           <li>
-            <div className="text-xs font-bold leading-[14.52px] text-[#787486]">
-              MY PROJECTS
+            <div className="flex items-center gap-x-24 text-xs font-bold leading-[14.52px] text-[#787486]">
+              <p>MY PROJECTS</p>
+              <AddSquare className="h-4 w-4 shrink-0 text-[#787486]" />
             </div>
-            <ul role="list" className="-mx-2 mt-2 space-y-1">
+            <ul role="list" className="-mx-2 mt-[21px] space-y-2">
               {teams.map((team) => (
                 <li key={team.name}>
                   <a
@@ -113,7 +117,7 @@ const Sidebar: FC = () => {
                       team.current
                         ? "bg-[#5030E5] bg-opacity-[0.08] text-gray-900"
                         : "text-[#787486] hover:text-gray-900 hover:bg-[#5030E5] hover:bg-opacity-[0.08]",
-                      "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold items-center"
+                      "group flex gap-x-3 rounded-md p-2 text-sm leading-5 font-semibold items-center"
                     )}
                   >
                     <span className={team.indicator}></span>
@@ -123,19 +127,33 @@ const Sidebar: FC = () => {
               ))}
             </ul>
           </li>
-          <li className="-mx-6 mt-auto">
-            <a
-              href="#"
-              className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-[rgba(80, 48, 229, 0.08)]"
-            >
-              <img
-                className="h-8 w-8 rounded-full bg-gray-50"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-              />
-              <span className="sr-only">Your profile</span>
-              <span aria-hidden="true">Tom Cook</span>
-            </a>
+          <li className="-mx-6 mt-auto p-6">
+            <div className="relative h-[200px] rounded-2xl bg-[#F5F5F5]">
+              <div className="absolute -top-9 left-16 w-[66px] h-[66px] rounded-full bg-[#F5F5F5]">
+                <div className="relative mx-auto mt-4 h-[33px] w-[33px] rounded-full bg-[#FCD64A] bg-opacity-70 blur-lg"></div>
+                <LampOn
+                  className="absolute bottom-4 left-4"
+                  size="32"
+                  color="#FBCB18"
+                  variant="Bulk"
+                />
+              </div>
+              <div className="pt-6 px-5 space-y-3 text-center">
+                <p className="text-center justify-center text-sm font-medium">
+                  Thoughts Time
+                </p>
+                <p className="text-xs text-[#787486] text-center justify-center">
+                  We don’t have any notice for you, till then you can share your
+                  thoughts with your peers.
+                </p>
+                <button
+                  type="button"
+                  className="rounded bg-white px-6 py-3 mt-2 text-sm font-medium focus-visible:outline-none focus:outline-none"
+                >
+                  Write a message
+                </button>
+              </div>
+            </div>
           </li>
         </ul>
       </nav>
